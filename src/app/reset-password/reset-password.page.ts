f44663f40db639705dabefc40b0d2e076a153355
afdhalmq <si23.afdhalmuttaqin@mhs.ubpkarawang.ac.id>
2026-06-13 16:52:40 +0700

    nyambungin button forget password di tampilan login

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import {
+  IonContent,
+  IonInput,
+  IonButton,
+  IonItem
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonInput,
+    IonButton,
+    IonItem
+  ]
 })
-export class ResetPasswordPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ResetPasswordPage {
 
 }
