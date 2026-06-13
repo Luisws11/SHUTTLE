3b7c6e4fcd896abe2488d525308d208817bd46a1
afdhalmq <si23.afdhalmuttaqin@mhs.ubpkarawang.ac.id>
2026-06-13 22:40:14 +0700

    Fix conflict in app.routes and update logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,98 +1,54 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     redirectTo: 'onboarding',
     pathMatch: 'full'
   },
-
   {
     path: 'onboarding',
-    loadComponent: () =>
-      import('./onboarding/onboarding.page')
-        .then(m => m.OnboardingPage)
+    loadComponent: () => import('./onboarding/onboarding.page').then(m => m.OnboardingPage)
   },
-
   {
     path: 'login',
-    loadComponent: () =>
-      import('./login/login.page')
-        .then(m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
   },
-
   {
     path: 'register',
-    loadComponent: () =>
-      import('./register/register.page')
-        .then(m => m.RegisterPage)
+    loadComponent: () => import('./register/register.page').then(m => m.RegisterPage)
   },
-
   {
     path: 'forgot-password',
-    loadComponent: () =>
-      import('./forgot-password/forgot-password.page')
-        .then(m => m.ForgotPasswordPage)
+    loadComponent: () => import('./forgot-password/forgot-password.page').then(m => m.ForgotPasswordPage)
   },
-
   {
     path: 'otp',
-    loadComponent: () =>
-      import('./otp/otp.page')
-        .then(m => m.OtpPage)
+    loadComponent: () => import('./otp/otp.page').then(m => m.OtpPage)
   },
-
   {
     path: 'reset-password',
-    loadComponent: () =>
-      import('./reset-password/reset-password.page')
-        .then(m => m.ResetPasswordPage)
+    loadComponent: () => import('./reset-password/reset-password.page').then(m => m.ResetPasswordPage)
   },
-
   {
     path: 'success',
-    loadComponent: () =>
-      import('./success/success.page')
-        .then(m => m.SuccessPage)
+    loadComponent: () => import('./success/success.page').then(m => m.SuccessPage)
   },
-
   {
     path: 'home',
-    loadComponent: () =>
-      import('./home/home.page')
-        .then(m => m.HomePage)
-  },
-
-  {
-  path: 'detail-lapangan',
-  loadComponent: () =>
-    import('./detail-lapangan/detail-lapangan.page')
-      .then(m => m.DetailLapanganPage)
-  },
-  {
-  path: 'booking-lapangan',
-  loadComponent: () =>
-    import('./booking-lapangan/booking-lapangan.page')
-      .then(m => m.BookingLapanganPage)
-  },
-  {
-    path: '**',
-    redirectTo: 'onboarding'
-  },
-  {
-    path: 'success',
-    loadComponent: () => import('./success/success.page').then( m => m.SuccessPage)
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
   },
   {
     path: 'detail-lapangan',
-    loadComponent: () => import('./detail-lapangan/detail-lapangan.page').then( m => m.DetailLapanganPage)
+    loadComponent: () => import('./detail-lapangan/detail-lapangan.page').then(m => m.DetailLapanganPage)
   },
   {
     path: 'booking-lapangan',
-    loadComponent: () => import('./booking-lapangan/booking-lapangan.page').then( m => m.BookingLapanganPage)
+    loadComponent: () => import('./booking-lapangan/booking-lapangan.page').then(m => m.BookingLapanganPage)
+  },
+  // Rute ** selalu ditaruh di paling bawah
+  {
+    path: '**',
+    redirectTo: 'onboarding'
   }
-
-
-
 ];
